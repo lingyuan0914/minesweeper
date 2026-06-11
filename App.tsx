d48a3589b6fc33c3a8f04b33fa389b29c5e52d91
lingyuan0914 <lingyuan0914@gmail.com>
2026-06-12 06:04:17 +0800

    feat: wrap App with ThemeProvider and GestureHandlerRootView

--- App.tsx
+++ App.tsx
@@ -1,19 +1,33 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { GameScreen } from './app/GameScreen'
+import { ThemeProvider, useTheme } from './components/ThemeProvider'
+
+function AppContent() {
+  const { theme, isDark } = useTheme()
+
+  return (
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
+      <StatusBar style={isDark ? 'light' : 'dark'} />
+      <GameScreen />
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="light" />
-      <GameScreen />
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <ThemeProvider>
+        <AppContent />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2c3e50'
-  }
+  },
 })
